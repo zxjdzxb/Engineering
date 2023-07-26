1395b19a3630a2b0a24d2f680344b119c322df7e
zxjdzxb <1660154581@qq.com>
2023-07-26 10:21:01 +0800

    chore: 路由

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,5 +31,6 @@ module.exports = {
         "vue"
     ],
     "rules": {
+
     }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup>
-import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
-const msg = ref('Hello Vite + Vue')
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld :msg="msg" />
+    <h1>Hello App!</h1>
+    <p>
+        <router-link to="/">Go to Home</router-link>
+    </p>
+
+    <router-view />
 </template>
 
 <style scoped>
